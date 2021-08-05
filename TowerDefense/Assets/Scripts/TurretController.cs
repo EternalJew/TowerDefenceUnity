a0922c2352eacd95fdbf1b812421142570bda48c
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-08-05 14:44:29 +0300

    addShootSystem

--- TowerDefense/Assets/Scripts/TurretController.cs
+++ TowerDefense/Assets/Scripts/TurretController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class TurretController : MonoBehaviour
 {
     private Transform target;
+    [Header("Attributes")]
     [SerializeField] private float range = 15f;
+    [SerializeField] private float fireRate = 1f;
+
+    [Header("Unity Setup Fields")]
     [SerializeField] private string enemyTag = "Enemy";
     public Transform partToRotate;
     [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    
+    private float fireCountDown = 0f;
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -40,12 +48,31 @@ public class TurretController : MonoBehaviour
     void Update()
     {
         if(target == null) 
-        return;
-        // Target look at
+            return;
+        // Target lock on
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountDown <= 0)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
     void OnDrawGizmosSelected()
     {
